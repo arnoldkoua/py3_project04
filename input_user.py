bbc18fb9d4c07fab986fd402354633e763a0192b
arnoldkoua <48394812+arnoldkoua@users.noreply.github.com>
2021-05-06 20:43:01 +0200

    Add files via upload

---
+++ input_user.py
@@ -0,0 +1,56 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "\"\"\"Le but de cet exercice est de demander à l'utilisateur de rentrer plusieurs informations comme son prenom, sa ville de naissance et son Age.\n",
+    "\n",
+    "Une fois que vous avez récuperer ces informations, il faut afficher ces informations à l'écran.\n",
+    "\n",
+    "veuillez enregistrer votre réponse dans un fichier input_user.py puis l'uploader sur github pour ensuite soumettre son lien de projet\"\"\"\n",
+    "\n",
+    "#Solution\n",
+    "\n",
+    "nom = input(\"Quel est votre nom ? \")\n",
+    "print()\n",
+    "prenom = input(\"Quel est votre prénom ? \")\n",
+    "print()\n",
+    "ville_naissance = input(\"Quel est votre ville de naissance ? \")\n",
+    "print()\n",
+    "age = input(\"Quel est votre âge ? \")\n",
+    "print()\n",
+    "print(\"Votre nom est : \", nom)\n",
+    "print()\n",
+    "print(\"Votre prénom est : \", prenom)\n",
+    "print()\n",
+    "print(\"Vous êtes né à\", ville_naissance)\n",
+    "print()\n",
+    "print(\"Et vous avez\", age, \"ans.\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
